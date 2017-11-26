7ac0983239a0d6d17ef3be314aa9dc8a79d75db1
maksymko658 <Jmaksy@ukr.net>
2017-11-26 04:54:51 +0200

    we have err message now

--- main.js
+++ main.js
@@ -9,10 +9,22 @@ document.querySelector('#zipForm').addEventListener
 			
 			
 			fetch(`http://api.zippopotam.us/us/${zip}`)
-			.then(response => response.json())
+			.then(response => {
+				if(response.status != 200){
+					document.querySelector('#output').innerHTML = 
+					`
+					<article class="message message-body is-danger">
+					Invalid Zipcode, try again.
+					</article>
+					`;
+					throw Err(response.statusText);
+				} else {
+					return response.json();
+				}
+			})
 			.then(data => {
 				console.log(data);
-			});
-
+			})
+			.catch(err => console.log(err));
 			e.preventDefault();
 		};
